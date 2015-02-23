e7b06a1cf0f2375cfc158c8680344b326aaa654c
Robert Coleman <github@robert.net.nz>
2015-02-23 19:45:40 +1300

    add created_at and index

--- app.rb
+++ app.rb
@@ -5,6 +5,11 @@ require 'sinatra/activerecord'
 autoload :Ping, 'app/models/ping'
 set :views, settings.root + '/app/views'
 
+get '/' do
+  @tokens = Ping.uniq.pluck(:token)
+  erb :index, locals: { tokens: @tokens }
+end
+
 post '/pings/:token' do
   content_type :json
   data = JSON.parse(request.body.read).merge(token: params[:token])
